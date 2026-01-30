00a123252cf6ce143615307bca74494be83ccb17
Abhijit-Kushwaha4 <gamingvninja@gmail.com>
2026-01-30 16:45:16 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+// /src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AIDemo } from './pages/AIDemo';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AIDemo />
+  </React.StrictMode>
+);
